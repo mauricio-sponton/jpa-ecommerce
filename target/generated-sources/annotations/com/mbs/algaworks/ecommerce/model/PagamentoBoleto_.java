00af6460562ce0510ff6d0c3492edea6b17de7a2
mauricio-sponton <sponton.lol@gmail.com>
2022-07-15 22:14:46 -0300

    Tipagem com Metamodel

---
+++ target/generated-sources/annotations/com/mbs/algaworks/ecommerce/model/PagamentoBoleto_.java
@@ -0,0 +1,16 @@
+package com.mbs.algaworks.ecommerce.model;
+
+import javax.annotation.processing.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(PagamentoBoleto.class)
+public abstract class PagamentoBoleto_ extends com.mbs.algaworks.ecommerce.model.Pagamento_ {
+
+	public static volatile SingularAttribute<PagamentoBoleto, String> codigoBarras;
+
+	public static final String CODIGO_BARRAS = "codigoBarras";
+
+}
+
