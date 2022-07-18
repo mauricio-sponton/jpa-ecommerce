08238abf01ef6a042794c9a1b230c51a1340b7f9
mauricio-sponton <sponton.lol@gmail.com>
2022-07-18 19:52:45 -0300

    Funções para String e Data com Criteria

--- src/main/java/com/mbs/algaworks/ecommerce/model/PagamentoBoleto.java
+++ src/main/java/com/mbs/algaworks/ecommerce/model/PagamentoBoleto.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -14,4 +16,7 @@ public class PagamentoBoleto extends Pagamento {
 
     @Column(name = "codigo_barras", length = 100)
     private String codigoBarras;
+
+    @Column(name = "data_vencimento")
+    private LocalDate dataVencimento;
 }
