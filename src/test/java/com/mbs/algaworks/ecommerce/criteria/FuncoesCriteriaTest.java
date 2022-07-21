4a05940ad1a4f49fabc082f7554602ae1f0d2062
mauricio-sponton <sponton.lol@gmail.com>
2022-07-21 02:41:49 -0300

    Funções para números, coleções, nativas e agregação com Criteria

--- src/test/java/com/mbs/algaworks/ecommerce/criteria/FuncoesCriteriaTest.java
+++ src/test/java/com/mbs/algaworks/ecommerce/criteria/FuncoesCriteriaTest.java
@@ -18,6 +18,110 @@ import java.util.List;
 public class FuncoesCriteriaTest extends EntityManagerTest {
 
     @Test
+    public void aplicarFuncaoAgregacao() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Object[]> criteriaQuery = criteriaBuilder.createQuery(Object[].class);
+        Root<Pedido> root = criteriaQuery.from(Pedido.class);
+
+        criteriaQuery.multiselect(
+                criteriaBuilder.count(root.get(Pedido_.id)),
+                criteriaBuilder.avg(root.get(Pedido_.total)),
+                criteriaBuilder.sum(root.get(Pedido_.total)),
+                criteriaBuilder.min(root.get(Pedido_.total)),
+                criteriaBuilder.max(root.get(Pedido_.total))
+        );
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(criteriaQuery);
+
+        List<Object[]> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+
+        lista.forEach(arr -> System.out.println(
+                "count: " + arr[0]
+                        + ", avg: " + arr[1]
+                        + ", sum: " + arr[2]
+                        + ", min: " + arr[3]
+                        + ", max: " + arr[4]));
+    }
+
+
+    @Test
+    public void aplicarFuncaoNativas() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Object[]> criteriaQuery = criteriaBuilder.createQuery(Object[].class);
+        Root<Pedido> root = criteriaQuery.from(Pedido.class);
+
+        criteriaQuery.multiselect(
+                root.get(Pedido_.id),
+                criteriaBuilder.function("dayname", String.class, root.get(Pedido_.dataCriacao))
+        );
+
+        criteriaQuery.where(criteriaBuilder.isTrue(
+                criteriaBuilder.function(
+                        "acima_media_faturamento", Boolean.class, root.get(Pedido_.total))));
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(criteriaQuery);
+
+        List<Object[]> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+
+        lista.forEach(arr -> System.out.println(
+                arr[0] + ", dayname: " + arr[1]));
+    }
+
+    @Test
+    public void aplicarFuncaoColecao() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Object[]> criteriaQuery = criteriaBuilder.createQuery(Object[].class);
+        Root<Pedido> root = criteriaQuery.from(Pedido.class);
+
+        criteriaQuery.multiselect(
+                root.get(Pedido_.id),
+                criteriaBuilder.size(root.get(Pedido_.itens))
+        );
+
+        criteriaQuery.where(criteriaBuilder.greaterThan(
+                criteriaBuilder.size(root.get(Pedido_.itens)), 1));
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(criteriaQuery);
+
+        List<Object[]> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+
+        lista.forEach(arr -> System.out.println(
+                arr[0]
+                        + ", size: " + arr[1]));
+    }
+
+    @Test
+    public void aplicarFuncaoNumero() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Object[]> criteriaQuery = criteriaBuilder.createQuery(Object[].class);
+        Root<Pedido> root = criteriaQuery.from(Pedido.class);
+
+        criteriaQuery.multiselect(
+                root.get(Pedido_.id),
+                criteriaBuilder.abs(criteriaBuilder.prod(root.get(Pedido_.id), -1)),
+                criteriaBuilder.mod(root.get(Pedido_.id), 2),
+                criteriaBuilder.sqrt(root.get(Pedido_.total))
+        );
+
+        criteriaQuery.where(criteriaBuilder.greaterThan(
+                criteriaBuilder.sqrt(root.get(Pedido_.total)), 10.0));
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(criteriaQuery);
+
+        List<Object[]> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+
+        lista.forEach(arr -> System.out.println(
+                arr[0]
+                        + ", abs: " + arr[1]
+                        + ", mod: " + arr[2]
+                        + ", sqrt: " + arr[3]));
+    }
+
+    @Test
     public void aplicarFuncaoData() {
         // current_date, current_time, current_timestamp
 
